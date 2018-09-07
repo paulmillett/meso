a63e7ef95fd139352001efcbd713bbe77875f303
Paul Millett <pmillett@uaf133460.ddns.uark.edu>
2018-09-07 10:50:07 -0500

    3D lattice-Boltzmann working

--- src/lattice_boltzmann/LBBaseClass.cpp
+++ src/lattice_boltzmann/LBBaseClass.cpp
@@ -18,20 +18,20 @@ LBBaseClass* LBBaseClass::LBFactory(const CommonParams& p,
                                     const GetPot& input_params)
 {
 
-    // -----------------------------------
-    // identify the requested object:
-    // -----------------------------------
+	// -----------------------------------
+	// identify the requested object:
+	// -----------------------------------
 
-    string lb_type = input_params("LBApp/type","mcmp2D");
+	string lb_type = input_params("LBApp/type","mcmp2D");
 
-    // -----------------------------------
-    // return the requested object:
-    // -----------------------------------
+	// -----------------------------------
+	// return the requested object:
+	// -----------------------------------
 
-    if (lb_type == "mcmp2D") return new mcmp2D(p,input_params);
+	if (lb_type == "mcmp2D") return new mcmp2D(p,input_params);
 	if (lb_type == "mcmp3D") return new mcmp3D(p,input_params);
 	
-    // if input file doesn't have a correct type return a nullptr
-    return NULL;
+	// if input file doesn't have a correct type return a nullptr
+	return NULL;
 
 }
